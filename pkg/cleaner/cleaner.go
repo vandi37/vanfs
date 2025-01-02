9fa7a97ef6e4ba8cf28b265f050c2ab81d95783f
Lev Vandi <Lev-person11@yandex.ru>
2025-01-02 15:55:31 +0300

    adding default clean

--- pkg/cleaner/cleaner.go
+++ pkg/cleaner/cleaner.go
@@ -10,13 +10,13 @@ type Cleaner map[string]func()
 
 func New() Cleaner {
 	clear := make(map[string]func())
-	clear["linux"] = func() {
-		cmd := exec.Command("clear")
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
-	clear["windows"] = func() {
-		cmd := exec.Command("cmd", "/c", "cls")
+	clear["default"] = func() {
+		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
@@ -27,5 +27,7 @@ func (c Cleaner) Clear() {
 	value, ok := c[runtime.GOOS]
 	if ok {
 		value()
+	} else {
+		c["default"]()
 	}
 }
